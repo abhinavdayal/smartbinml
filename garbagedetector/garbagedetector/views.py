6ac93727d04654f630e28a1b458fbf2c73f760b4
abhinavdayal <abhinav.dayal@hotmail.com>
2020-08-01 16:29:00 +0530

    f

--- garbagedetector/garbagedetector/views.py
+++ garbagedetector/garbagedetector/views.py
@@ -1,6 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from PIL import Image
+from io import BytesIO
 from . import classifier
 from base64 import b64decode
 
@@ -14,7 +15,7 @@ class CaffeDetector(APIView):
             height = request.data['height']
             mode = request.data['mode']
             
-            sample = Image.frombytes(mode, (width,height), image_data, 'raw')
+            sample = Image.open(BytesIO(image_data))
             response = classifier.getPredictionsFor([sample], net, mean)
             return Response(response, status=201)
         except Exception as e:
